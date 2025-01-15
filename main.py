fa0874f08e6488d79c79acf686c769d68f3ff5fc
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2025-01-15 10:39:19 -0500

    fghgj

---
+++ main.py
@@ -0,0 +1,22 @@
+# Lucas Brinks
+# 1/15/25
+# Dictionary starter
+my_dict = {'name': 'Lucas', 'age': 16}
+
+print(my_dict)
+
+my_dict['age'] =17
+
+print(my_dict)
+
+contacts = {
+    'Mother of mine': '231-735-3777',
+    'James Games': '231-384-7734',
+    'Father of mine': '231-649-1053',
+    'Sam': '231-132-3123',
+    'Jhon': '991-191-9911'
+}
+
+contacts['Father of mine'] = '294-582-5839'
+
+print(contacts)
